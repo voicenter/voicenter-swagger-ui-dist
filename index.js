7c9193d1a1b39d30c0ddcf90f39e76401ec15d00
serhii mamedov <serhiy.ma@starkey.co.il>
2020-06-12 15:36:32 +0300

    updated version

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const { readdirSync, copyFileSync, existsSync, mkdirSync } = require('fs');
  * Will replace destination folder with dist files
  * @param {string} destination - absolute path
  */
-const replaceWithDist = destination => {
+exports.replaceWithDist = destination => {
   if (!existsSync(destination)) mkdirSync(destination);
 
   const files = readdirSync(resolve('./dist'));
 
   files.forEach(file => copyFileSync(resolve('./dist', file), `${destination}/${file}`));
-}
-
-module.exports = replaceWithDist;
+};
